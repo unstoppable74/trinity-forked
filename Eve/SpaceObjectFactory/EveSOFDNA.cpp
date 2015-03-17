c4943a417c7e23fcf6c3c0dfb7188e4fba294013
filipppavlov <filipppavlov@users.noreply.github.com>
2015-03-17 10:08:57 +0000

    Fixed compile error on Android
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 870283]

--- Eve/SpaceObjectFactory/EveSOFDNA.cpp
+++ Eve/SpaceObjectFactory/EveSOFDNA.cpp
@@ -146,7 +146,7 @@ float EveSOFDNA::GetDirtLevel() const
 		// has only one parameter: a float!
 		if( dirtLevelCommandArgs.size() == 1 )
 		{
-			return std::stof( dirtLevelCommandArgs[0] );
+			return atof( dirtLevelCommandArgs[0].c_str() );
 		}
 	}
 	return -10.f;
