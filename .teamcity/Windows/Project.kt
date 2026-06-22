269d3298afd7da1409cde4dd4802b321708c9e7e
ccptoebeans <ccptoebeans@users.noreply.github.com>
2026-06-22 16:03:41 +0000

    Add License and Readme files, Fix Windows.kt bootstrap step & remove .github/ dir

--- .teamcity/Windows/Project.kt
+++ .teamcity/Windows/Project.kt
@@ -50,6 +50,8 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String) 
         param("env.CTEST_JUNIT_OUTPUT_FILE", "ctest_results.xml")
         select("env.VISUAL_STUDIO_PLATFORM_TOOLSET", "v141", label = "Visual Studio Platform Toolset", description = "Specify the toolset for the build. e.g. v141 or v143.",
                 options = listOf("v141 (2017)" to "v141", "v143 (2022)" to "v143"))
+        param("VS_DEV_BAT_SWITCHES", "-arch=x64")
+        param("env.VSDEV_BAT_PATH", "%%ProgramFiles(x86)%%/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/vsdevcmd.bat")
         param("env.CMAKE_BUILD_TARGETS", "all")
         param("env.CMAKE_INSTALL_PREFIX", ".build-artifact")
         param("env.CMAKE_CONFIG_TYPE", configType)
@@ -91,7 +93,7 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String) 
             scriptContent = """
                 REM unfortunately ninja does not find the VS environment otherwise
                 REM NB: the exported PATH also contains the location where we installed sentry-cli, e.g. teamcity.agent.work.dir
-                call "%%ProgramFiles(x86)%%\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\vsdevcmd.bat" -arch=x64
+                call "%env.VSDEV_BAT_PATH%" %VS_DEV_BAT_SWITCHES%
                 echo ##teamcity[setParameter name='env.INCLUDE' value='%%INCLUDE%%']
                 echo ##teamcity[setParameter name='env.LIB' value='%%LIB%%']
                 echo ##teamcity[setParameter name='env.LIBPATH' value='%%LIBPATH%%']
---
+++ README.md
@@ -0,0 +1,21 @@
+# Carbon trinity
+Rendering engine for the Carbon Game Engine
+
+## 🛠️ Building
+
+Build using provided `CMakeLists.txt` in the repository root.
+
+## 🤝 Contributing
+Contribution follows the standard GIT PR model.
+
+By submitting a pull request or otherwise contributing to this project, you agree to license your contribution under the (to be determined) License, and you confirm that you have the right to do so.
+
+## 📄 License and Legal Notices
+
+© 2026 CCP Games
+
+This software is provided by CCP Games and does not include or distribute any third-party libraries or frameworks.
+
+Trademark Notice: CCP Games is a trademark of CCP ehf.
+
+This project is licensed under the [MIT License](LICENSE.md). Nothing in the [MIT License](LICENSE.md) grants any rights to CCP Games' trademarks or game content.
